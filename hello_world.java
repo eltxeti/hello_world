1acaddefe571ec36cf8ebf1646ca0e202fd296dc
eltxeti <eltxeti@hotmail.com>
2017-10-02 19:02:37 +0200

    versio 1.2

--- hello_world.java
+++ hello_world.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT");
     }
 
 }
